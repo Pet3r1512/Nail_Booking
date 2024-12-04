9d37906828efbb0651629a14bc32ac9253e3fd46
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-12-04 20:43:12 +0700

    Build booking form (#3)
    * style: avoid full screen height
    
    * style: remove unused
    
    * chore: shadcn's input component
    
    * fix: wrong variable name's meaning
    
    * feat: booking form

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
+import BookingForm from "./components/Forms/BookingForm";
 import Page from "./components/Layout/Page";
 
 function App() {
   return (
-    <Page className="flex items-center justify-center min-h-screen">
-      <></>
+    <Page className="flex items-center justify-center py-10">
+      <BookingForm />
     </Page>
   );
 }
---
+++ src/components/Forms/BookingForm.tsx
@@ -0,0 +1,30 @@
+import { Input } from "@/components/ui/input";
+
+export default function BookingForm() {
+  return (
+    <section className="max-w-lg w-full bg-white shadow-2xl min-h-96 rounded-2xl p-5 lg:px-5 lg:py-14 flex flex-col">
+      <form className="h-full w-full flex flex-col gap-y-6 items-center flex-1">
+        <p className="text-lg font-bold text-primary">{"Đặt Lịch"}</p>
+        <div className="w-full space-y-3">
+          <label htmlFor="name">Họ và Tên</label>
+          <Input
+            className="inputs ring-0 border-2 border-gray-400 focus-visible:ring-offset-0 focus-visible:ring-0"
+            type="text"
+            placeholder="Tên của bạn"
+          />
+        </div>
+        <div className="w-full space-y-3">
+          <label htmlFor="phone">Số điện thoại</label>
+          <Input
+            className=" inputs ring-0 border-2 border-gray-400 focus-visible:ring-offset-0 focus-visible:ring-0"
+            type="tel"
+            placeholder="090*******"
+          />
+        </div>
+      </form>
+      <button className="bg-green-500 text-white px-3 py-2 rounded-2xl font-semibold w-fit">
+        {"Xác Nhận"}
+      </button>
+    </section>
+  );
+}
