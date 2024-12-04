fbe26314f8bf9ac57bc2beffabebaded3f1790e7
Peter Pham <pttp15122002@gmail.com>
2024-12-04 20:05:51 +0700

    feat: add banner on mobiles

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { cn } from "../../lib/utils";
 import Header from "./Header";
+import { MapPin } from "lucide-react";
 
 export default function Page({
   children,
@@ -13,6 +14,9 @@ export default function Page({
 }) {
   return (
     <main className="bg-[#f8f9fa] bg-cover bg-center h-full">
+      <p className="flex items-center justify-center gap-x-2 cursor-default font-semibold text-sm lg:hidden py-2 bg-primary text-white">
+        <MapPin /> 73/14 Đường Số 1, p.11, q.Gò Vấp
+      </p>
       <Header />
       <section
         className={cn(
