c19b0579b7bcd2ec0b0d411bba5b9228b77b09f3
Peter Pham <pttp15122002@gmail.com>
2024-12-04 20:05:26 +0700

    style: responsive for mobiles

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -3,12 +3,12 @@ import Sidebar from "./Sidebar";
 
 export default function Header() {
   return (
-    <header className="max-w-7xl mx-auto py-5 lg:py-8 flex items-center justify-between">
+    <header className="max-w-7xl mx-auto py-5 lg:py-8 flex items-center justify-between px-5 lg:px-0">
       <Sidebar />
-      <p className="font-extrabold lg:text-3xl text-primary cursor-default">
+      <p className="font-extrabold text-lg md:text-xl lg:text-3xl text-primary cursor-default">
         Nail.Booking
       </p>
-      <p className="flex items-center gap-x-2 cursor-default font-semibold lg:text-lg">
+      <p className="hidden lg:flex items-center gap-x-2 cursor-default font-semibold lg:text-lg">
         <MapPin /> 73/14 Đường Số 1, p.11, q.Gò Vấp
       </p>
     </header>
