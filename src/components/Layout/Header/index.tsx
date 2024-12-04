5ea1ab6248462d81d335bcab088fa8f68c95ebd4
Peter Pham <pttp15122002@gmail.com>
2024-12-04 20:08:30 +0700

    feat: update logo

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="max-w-7xl mx-auto py-5 lg:py-8 flex items-center justify-between px-5 lg:px-0">
       <Sidebar />
       <p className="font-extrabold text-lg md:text-xl lg:text-3xl text-primary cursor-default">
-        Nail.Booking
+        Điệp Tăng Nail
       </p>
       <p className="hidden lg:flex items-center gap-x-2 cursor-default font-semibold lg:text-lg">
         <MapPin /> 73/14 Đường Số 1, p.11, q.Gò Vấp
