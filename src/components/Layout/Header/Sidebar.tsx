3f65acb00d4cdee6078f4b21ccb4aa1e05a3250a
Peter Pham <pttp15122002@gmail.com>
2024-12-04 20:06:05 +0700

    style: remove unused

--- src/components/Layout/Header/Sidebar.tsx
+++ src/components/Layout/Header/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Menu } from "lucide-react";
 export default function Sidebar() {
   return (
     <Drawer direction="left">
-      <DrawerTrigger className="lg:hidden relative top-8 left-5">
+      <DrawerTrigger className="lg:hidden">
         <Menu />
       </DrawerTrigger>
       <DrawerContent className="h-[100dvh] w-2/3 !rounded-l-none rounded-r-xl px-5 py-12 flex flex-col gap-y-5"></DrawerContent>
