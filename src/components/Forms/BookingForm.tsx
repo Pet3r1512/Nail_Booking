b2dae7f6f2ce4c147fdc04e9a998e64bf928bbbe
Peter Pham <pttp15122002@gmail.com>
2024-12-05 08:23:29 +0700

    feat: add date picker

--- src/components/Forms/BookingForm.tsx
+++ src/components/Forms/BookingForm.tsx
@@ -1,10 +1,13 @@
 import { Input } from "@/components/ui/input";
+import { DatePicker } from "../DatePicker";
 
 export default function BookingForm() {
   return (
     <section className="max-w-lg w-full bg-white shadow-2xl min-h-96 rounded-2xl p-5 lg:px-5 lg:py-14 flex flex-col">
-      <form className="h-full w-full flex flex-col gap-y-6 items-center flex-1">
-        <p className="text-lg font-bold text-primary">{"Đặt Lịch"}</p>
+      <form className="h-full w-full flex flex-col gap-y-6 items-center flex-1 py-5">
+        <p className="text-xl md:text-2xl lg:text-4xl font-bold text-primary">
+          {"Đặt Lịch"}
+        </p>
         <div className="w-full space-y-3">
           <label htmlFor="name">Họ và Tên</label>
           <Input
@@ -20,6 +23,10 @@ export default function BookingForm() {
             type="tel"
             placeholder="090*******"
           />
+        </div>
+        <div className="w-full flex flex-col gap-y-3">
+          <label htmlFor="date">Ngày đặt hẹn</label>
+          <DatePicker />
         </div>
       </form>
       <button className="bg-green-500 text-white px-3 py-2 rounded-2xl font-semibold w-fit">
