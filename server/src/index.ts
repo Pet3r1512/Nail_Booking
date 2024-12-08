a6d989c7cf4dbba07c2320913386b3d8b332d982
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-12-08 15:02:05 +0700

    Create user api with drizzle and neon db (#9)
    * ops: commands for drizzle orm
    
    * ops: drizzle orm db instance
    
    * ops: create new customer
    
    * remove unused
    
    * ops: update to ts instead of tsx
    
    * ops: drizzle migrate

---
+++ server/src/index.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Hono } from "hono";
+import { customers } from "./db/schema";
+import db from "./db";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.json({
+    message: "ok",
+  });
+});
+
+app.post("/customers", async (c) => {
+  try {
+    const { name, phoneNumber } = await c.req.json();
+
+    if (
+      !name ||
+      typeof name !== "string" ||
+      !phoneNumber ||
+      typeof phoneNumber !== "string"
+    ) {
+      return c.json(
+        {
+          error: "Invalid credentials",
+        },
+        400,
+      );
+    }
+
+    const customer = { name, phoneNumber };
+
+    const result = await db.insert(customers).values(customer).returning();
+
+    return c.json(
+      {
+        customer: result[0],
+      },
+      201,
+    );
+  } catch (e) {
+    return c.json(
+      {
+        error: (e as Error).message || "An unexpected error occurred",
+      },
+      400,
+    );
+  }
+});
+
+export default app;
