130abf8cbf104584231edf91fa0ffce460dc34a8
Peter Pham <pttp15122002@gmail.com>
2024-12-09 19:53:04 +0700

    ops: submit form by button and show toast

--- src/components/Forms/_index.tsx
+++ src/components/Forms/_index.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Input } from "@/components/ui/input";
 import { useEffect, useState } from "react";
-import { useFormStore } from "@/store/formStore";
+import { initFormData, useFormStore } from "@/store/formStore";
 import useDebounce from "@/hooks/useDebounce";
 import { cn } from "@/lib/utils";
 import SlotPicker from "./SlotPicker";
 import PhoneInput from "./PhoneInput";
+import { useToast } from "@/hooks/use-toast";
+import { LoaderCircle } from "lucide-react";
+import { Toaster } from "../ui/toaster";
 
 export default function BookingForm() {
   const { updateField } = useFormStore();
@@ -17,6 +21,9 @@ export default function BookingForm() {
     time: "",
   });
   const [allow, setAllow] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { toast } = useToast();
   const debouncedName = useDebounce(name, 500);
 
   useEffect(() => {
@@ -28,10 +35,48 @@ export default function BookingForm() {
     if (form.name === "" || form.phoneNumber === "" || form.time === "") {
       setAllow(false);
     } else {
-      console.log(form);
       setAllow(true);
     }
   }, [form]);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      // const response = await fetch("http://127.0.0.1:8787/bookings/", {
+      const response = await fetch(
+        "https://server.pttp15122002.workers.dev/bookings/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        },
+      );
+      if (!response.ok) {
+        setError("Thất bại");
+        toast({
+          variant: "destructive",
+          className: "bg-red-500 text-white",
+          title: "Thất Bại",
+          description: "Xin vui lòng thử lại!",
+        });
+      }
+      const data = await response.json();
+      if (data.success) {
+        toast({
+          className: "bg-green-500 text-white",
+          title: "Thành Công",
+          description: "Xin cám ơn quý khách",
+        });
+        setForm(initFormData);
+      }
+    } catch (error) {
+      setError("Thất bại");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <section className="max-w-lg w-full bg-white shadow-2xl min-h-96 rounded-2xl p-5 lg:px-5 lg:py-14 flex flex-col">
@@ -55,15 +100,21 @@ export default function BookingForm() {
       </form>
       <button
         disabled={!allow}
+        onClick={handleSubmit}
         className={cn(
-          " text-white px-3 py-2 rounded-2xl font-semibold",
+          " text-white px-3 py-2 rounded-2xl font-semibold flex justify-center items-center",
           allow
             ? "bg-green-500 cursor-pointer"
             : "bg-gray-500 cursor-not-allowed",
         )}
       >
-        {"Xác Nhận"}
+        {loading ? (
+          <LoaderCircle className="animate-spin" />
+        ) : (
+          <p>{"Xác Nhận"}</p>
+        )}
       </button>
+      <Toaster />
     </section>
   );
 }
