3b1c79aa1df518c8a7b7efe5b72477aeebb38953
Peter Pham <pttp15122002@gmail.com>
2024-12-09 12:26:33 +0700

    fix: use tokens from dev.vars instead

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 
-config({ path: ".env" });
+config({ path: ".dev.vars" });
 
 const postgresql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: postgresql });
