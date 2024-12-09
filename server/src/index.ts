ca4b6ad87f7a41dce29cad0e47a3dca4e181208f
Peter Pham <pttp15122002@gmail.com>
2024-12-09 15:24:02 +0700

    ops: api for get all bookings in the specific day

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { Hono } from "hono";
-import { customers } from "./db/schema";
+import { bookings, customers } from "./db/schema";
+import { eq } from "drizzle-orm";
+import { cors } from "hono/cors";
 
 export type Env = {
   DATABASE_URL: string;
@@ -9,11 +11,24 @@ export type Env = {
 
 const app = new Hono<{ Bindings: Env }>();
 
+app.use("/*", cors());
+app.use(
+  "/*",
+  cors({
+    origin: "http://localhost:5173", // Ensure this matches your Vite server's origin
+    allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
+
 app.get("/", async (c) => {
   try {
-    const sql = neon(c.env.DATABASE_URL);
+    const postgresql = neon(c.env.DATABASE_URL);
 
-    const db = drizzle(sql);
+    const db = drizzle(postgresql);
 
     const result = await db.select().from(customers);
 
@@ -28,6 +43,31 @@ app.get("/", async (c) => {
       },
       400,
     );
+  }
+});
+
+app.post("/bookings/:date", async (c) => {
+  const checkDate: string = c.req.param("date");
+  console.log(checkDate);
+
+  try {
+    const postgresql = neon(c.env.DATABASE_URL);
+
+    const db = drizzle(postgresql);
+
+    const result = await db
+      .select()
+      .from(bookings)
+      .where(eq(bookings.date, checkDate));
+    console.log(result);
+    if (result.length >= 4) {
+      return c.json({ available: false });
+    }
+
+    return c.json({ available: true, bookings: result }, 200);
+  } catch (error) {
+    console.error("Error checking bookings:", error);
+    return c.json({ error: "Something went wrong." }, 500);
   }
 });
 
