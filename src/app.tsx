9724287795b27332b2039008d34f08681955e828
Peter Pham <pttp15122002@gmail.com>
2024-12-09 19:52:34 +0700

    feat: wrap app in toast provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,14 @@
 import BookingForm from "./components/Forms/_index";
 import Page from "./components/Layout/Page";
+import { ToastProvider } from "./components/ui/toast";
 
 function App() {
   return (
-    <Page className="flex items-center justify-center py-10">
-      <BookingForm />
-    </Page>
+    <ToastProvider>
+      <Page className="flex items-center justify-center py-10">
+        <BookingForm />
+      </Page>
+    </ToastProvider>
   );
 }
 
