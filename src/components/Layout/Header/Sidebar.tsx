52bc1b3820fd2135ad15efe8cb5d97937e82110d
Peter Pham <pttp15122002@gmail.com>
2024-12-10 07:38:21 +0700

    add phone number to sidebar

--- src/components/Layout/Header/Sidebar.tsx
+++ src/components/Layout/Header/Sidebar.tsx
@@ -7,7 +7,12 @@ export default function Sidebar() {
       <DrawerTrigger className="lg:hidden">
         <Menu />
       </DrawerTrigger>
-      <DrawerContent className="h-[100dvh] w-2/3 !rounded-l-none rounded-r-xl px-5 py-12 flex flex-col gap-y-5"></DrawerContent>
+      <DrawerContent className="h-[100dvh] w-2/3 !rounded-l-none rounded-r-xl px-5 py-12 flex flex-col gap-y-5">
+        <div className="flex items-center gap-x-2.5 font-semibold text-lg">
+          <p className="text-primary font-bold">Zalo </p>
+          <p>0918234767</p>
+        </div>
+      </DrawerContent>
     </Drawer>
   );
 }
